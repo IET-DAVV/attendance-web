2ef42cd764309d8809ea6ced69474421adac5c76
Anurag Pal <apal895@gmail.com>
2023-08-23 11:52:17 +0530

    file name correction cacse sensitive

--- src/utils/api/services/index.ts
+++ src/utils/api/services/index.ts
@@ -2,4 +2,4 @@ export { default as attendanceServices } from "./attendance";
 export { default as studentServices } from "./students";
 export { default as constantsServices } from "./constants";
 export { default as facultiesServices } from "./faculties";
-export { default as timeTableServices } from "./timetable";
+export { default as timeTableServices } from "./timeTable";
